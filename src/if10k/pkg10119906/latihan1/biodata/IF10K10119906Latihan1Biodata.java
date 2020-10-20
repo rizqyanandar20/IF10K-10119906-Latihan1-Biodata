d666832c4a098ca99182f28841b5074126c22458
rizqyanandar20 <rizqyananda.rusmana@gmail.com>
2020-10-21 06:27:28 +0700

    first commit

---
+++ src/if10k/pkg10119906/latihan1/biodata/IF10K10119906Latihan1Biodata.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if10k.pkg10119906.latihan1.biodata;
+
+/**
+ *
+ * @author senenngahenen
+ * Nama      : Rizqy Ananda Rusmana
+ * NIM       : 10119906
+ * Kelas     : IF-10K
+ * Deskripsi : Perintah menampilkan value ke layar.
+ */
+public class IF10K10119906Latihan1Biodata {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        System.out.println("BIODATA ROCKSTAR" + "\n" + "----------------");
+        System.out.println("Nama\t\t: Rizqy Ananda Rusmana");
+        System.out.println("Alamat\t\t: Jl. Tubagus Ismail No.26e");
+        System.out.println("Tempat Lahir\t: Bandung");
+        System.out.println("Tanggal Lahir\t: 30 Agustus 1998");
+        System.out.println("----------------");
+        System.out.println("Hobi\t\t: Renang & Lari");
+        System.out.println("Makes\t\t: Sate");
+        System.out.println("Mikes\t\t: Cendol");
+        
+    }
+    
+}
